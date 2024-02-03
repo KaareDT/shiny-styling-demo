5b751c8b6fd034cba8f3e4e4a3881a20ad73c5cd
Kaare D. Tranaes <cxb592@ku.dk>
2024-02-03 20:05:39 +0100

    freshed up with bslib and thematic

--- bare-app/app.R
+++ bare-app/app.R
@@ -1,40 +1,68 @@
 library(shiny)
 library(tidyverse)
+library(bslib)
+library(bsicons)
+library(thematic)
+library(gitlink)
 
 source("setup.R")
 source("helpers.R")
 
-ui <- fluidPage(
-  titlePanel("Effectiveness of DemoCo App Free Trial by Customer Profile"),
-  sidebarLayout(
-    sidebarPanel(
-      sidebar_content
-    ),
-    mainPanel(
-      "Conversions over time",
-      plotOutput("line"),
+thematic_shiny()
 
-      "Conversion rates",
-      plotOutput("bar"),
+ui <- page_sidebar(
 
-      "Recommended Trial",
-      textOutput("recommended_eval"),
+  ribbon_css("https://github.com/KaareDT/shiny-styling-demo"),
 
+  theme = bs_theme(version = 5,
+                   bootswatch = "darkly",
+                   success = "#86C7ED", # customize colors used in theme-settings below
+                   "table-color" = "#86C7ED"), # customize colors of table text
 
+  title = "Effectiveness of DemoCo App Free Trial by Customer Profile",
+  sidebar = sidebar(
+    class = "bg-secondary", #change color of sidebar
+    sidebar_content,
+    HTML('<img src="logo.png" width="100%" height="auto">')
+  ),
+  layout_columns(
+      card(card_header("Conversions over time",class = "text-success"),
+      plotOutput("line")),
 
+    card(card_header("Conversion rates", class = "text-success"),
+                     plotOutput("bar")),
 
+    value_box(title = "Recommended Trial",
+                     textOutput("recommended_eval"),
+              showcase = bs_icon("stars"),
+              theme = "success"),
 
-      "Conversion rates by subgroup",
-      tableOutput("table")
-    )
+    value_box(title = "Users", class = "text-success",
+                     textOutput("number_of_users"),
+              showcase = bs_icon("people"),
+              theme = "secondary"),
+
+    value_box(title = "Avg Spend", class = "text-success",
+                     textOutput("average_spend"),
+              showcase = bs_icon("coin"),
+              theme = "secondary"),
+
+    card(card_header("Conversion rates by subgroup"),
+                     tableOutput("table")),
+
+    col_widths = c(8,4
+                   ,4,4,4,
+                   12),
+    row_heights = c(4, 1.5, 3)
   )
 )
 
 
 
-
 # Define the Shiny server function
 server <- function(input, output) {
+
+  #bs_themer() # toggle on to get interactive themer
 
   # Filter data according to inputs
   selected_industries <-
@@ -49,15 +77,15 @@ server <- function(input, output) {
   selected_data <-
     reactive({
       filter_users(selected_industries(),
-                       selected_propensities(),
-                       selected_contracts())
+                   selected_propensities(),
+                   selected_contracts())
     })
 
   selected_data_by_group <-
     reactive({
       filter_users_by_group(selected_industries(),
-                                selected_propensities(),
-                                selected_contracts())
+                            selected_propensities(),
+                            selected_contracts())
     })
 
 
